5aba6cc8d4868aa9f5032d8035837810d131d90b
Joshua Lee <joshlee1117@yahoo.com>
2023-09-10 01:35:10 +0000

    Annotate all methods in MessageService

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -24,7 +24,13 @@ public class MessageService {
         this.messageDAO = messageDAO;
     }
 
-    // Use MessageDAO to insert a new message into the message table
+    /**
+     * Use the MessageDAO to insert a new message into the database.
+     * @param message an object representing a new message.
+     * @return the newly added message if the insert operation was successful, including 
+     *         its message_id. Return null if the message failed to meet the message 
+     *         requirements or if the insert operation was unsuccessful.
+     */
     public Message insertMessage(Message message) {
         if (message.getMessage_text().isEmpty()) {
             return null;
@@ -37,22 +43,45 @@ public class MessageService {
         return messageDAO.insertMessage(message);
     }
 
-    // Use MessageDAO to retrieve all messages
+    /**
+     * Use the MessageDAO to retrieve a List containing all messages from the database.
+     * @return all messages in the database.
+     */
     public List<Message> getAllMessages() {
         return messageDAO.getAllMessages();
     }
 
-    // Use MessageDAO to retrieve a message by its message id
+    /**
+     * Use the MessageDAO to retrieve a message from the database.
+     * @param message_id the id of the message to be retrieved
+     * @return the message if the get operation was successful. 
+     *         Return null if the get operation was unsuccessful.
+     */
     public Message getMessageById(int message_id) {
         return messageDAO.getMessageById(message_id);
     }
 
+    /**
+     * Use the MessageDAO to delete a message from the database.
+     * @param message_id the id of the message to be deleted
+     * @return the now deleted message if the message was found in the database.
+     *         Return null if a message was not found with the given message id.
+     */
     public Message deleteMessage(int message_id) {
         Message deletedMessage = messageDAO.getMessageById(message_id);
         messageDAO.deleteMessage(message_id);
         return deletedMessage;
     }
 
+    /**
+     * Use the MessageDAO to update a message from the database.
+     * @param message_id the id of the message to be updated.
+     * @param message_text the text string containing all the data that should replace
+     *         the text string contained by the existing message.
+     * @return the now updated message if the message was found in the database.
+     *         Return null if the message text does not meet the requirements for a 
+     *         message text or a message was not found with the given message id.
+     */
     public Message updateMessage(int message_id, String message_text) {
         if (messageDAO.getMessageById(message_id) == null) {
             return null;
@@ -66,6 +95,12 @@ public class MessageService {
         return messageDAO.getMessageById(message_id);
     }
 
+    /**
+     * Use the MessageDAO to retrieve a List containing all messages posted by a 
+     * given account from the database.
+     * @param account_id the id of the account having its messages retrieved.
+     * @return all messages posted by the account in the database.
+     */
     public List<Message> getUserMessages(int account_id) {
         return messageDAO.getMessagesByAccountId(account_id);
     }
